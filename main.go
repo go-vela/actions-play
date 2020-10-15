e7d3ead558cc0b84986e5737ab9cfe403d6d545e
Jordan <Jordan.Brockopp@target.com>
2020-10-15 09:58:43 -0500

    use ahmetb/govvv for version information

--- main.go
+++ main.go
@@ -6,30 +6,38 @@ import (
 )
 
 var (
-	// App represents the semantic version information for the application.
-	App string
 	// Arch represents the architecture information for the application.
 	Arch = runtime.GOARCH
-	// Built represents the build date information for the application.
-	Built string
-	// Git represents the git information for the application.
-	Git string
-	// Lang represents the go information for the application.
-	Lang string
+	// BuildDate represents the build date information for the application.
+	BuildDate string
+	// GitBranch represents the git branch information for the application.
+	GitBranch string
+	// GitCommit represents the git commit information for the application.
+	GitCommit string
+	// GitState represents the git state information for the application.
+	GitState string
+	// GitSummary represents the git summary information for the application.
+	GitSummary string
 	// OS represents the operating system information for the application.
 	OS = runtime.GOOS
+	// Version represents the semantic version information for the application.
+	Version string
 )
 
 func main() {
-	fmt.Println("App: ", App)
-
 	fmt.Println("Arch: ", Arch)
 
-	fmt.Println("Built: ", Built)
+	fmt.Println("Build Date: ", BuildDate)
 
-	fmt.Println("Git: ", Git)
+	fmt.Println("Git Branch: ", GitBranch)
 
-	fmt.Println("Lang: ", Lang)
+	fmt.Println("Git Commit: ", GitCommit)
+
+	fmt.Println("Git State: ", GitState)
+
+	fmt.Println("Git Summary: ", GitSummary)
 
 	fmt.Println("OS: ", OS)
+
+	fmt.Println("Version: ", Version)
 }

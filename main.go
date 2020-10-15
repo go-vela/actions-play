7b93e110b2a0234ec395929939e898d89c74075d
Jordan <Jordan.Brockopp@target.com>
2020-10-15 09:20:19 -0500

    embed os and arch into binary

--- main.go
+++ main.go
@@ -1,23 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"runtime"
+)
 
 var (
 	// App represents the semantic version information for the application.
 	App string
 	// Arch represents the architecture information for the application.
-	Arch string
+	Arch = runtime.GOARCH
 	// Built represents the build date information for the application.
 	Built string
 	// Git represents the git information for the application.
 	Git string
 	// Lang represents the go information for the application.
 	Lang string
+	// OS represents the operating system information for the application.
+	OS = runtime.GOOS
 )
 
 func main() {
 	fmt.Println("App: ", App)
-  
+
 	fmt.Println("Arch: ", Arch)
 
 	fmt.Println("Built: ", Built)
@@ -25,4 +30,6 @@ func main() {
 	fmt.Println("Git: ", Git)
 
 	fmt.Println("Lang: ", Lang)
+
+	fmt.Println("OS: ", OS)
 }
